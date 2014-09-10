dcd3a508d280baeb27f406a139cb168c8cf38a05
Kyle Kelley <rgbkrk@gmail.com>
2014-09-10 18:39:28 +0000

    Make sure the notebook is started with python3

--- notebook/notebook.sh
+++ notebook/notebook.sh
@@ -12,7 +12,7 @@ fi
 
 # Create the hash to pass to the IPython notebook, but don't export it so it doesn't appear
 # as an environment variable within IPython kernels themselves
-HASH=$(python -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))")
+HASH=$(python3 -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))")
 unset PASSWORD
 
 ipython notebook --no-browser --port 8888 --ip=* --certfile=$PEM_FILE --NotebookApp.password="$HASH"
