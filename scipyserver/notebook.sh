95b552460f107109ec319fb84ce54b95a5195ca6
Kyle Kelley <rgbkrk@gmail.com>
2014-09-12 01:40:44 +0000

    IPython 2 explicitly

--- scipyserver/notebook.sh
+++ scipyserver/notebook.sh
@@ -15,4 +15,4 @@ fi
 HASH=$(python3 -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))")
 unset PASSWORD
 
-ipython notebook --no-browser --port 8888 --ip=* --certfile=$PEM_FILE --NotebookApp.password="$HASH"
+ipython2 notebook --no-browser --port 8888 --ip=* --certfile=$PEM_FILE --NotebookApp.password="$HASH"
