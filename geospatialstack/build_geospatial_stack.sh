c60efb744d3b16c297036deed843d27d824956f8
epifanio <epiesasha@me.com>
2014-09-17 06:27:34 -0400

    Update build_geospatial_stack.sh

--- geospatialstack/build_geospatial_stack.sh
+++ geospatialstack/build_geospatial_stack.sh
@@ -145,7 +145,7 @@ cd grass7_trunk
 #LD_LIBRARY_PATH=$PREFIX/lib/ CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib ./configure --with-freetype-includes=/usr/include/freetype2/ --with-geos=$PREFIX/bin/geos-config --with-netcdf=$PREFIX/bin/nc-config --with-proj-data=$PREFIX/share/proj/ --with-postgres=yes --with-sqlite --with-pthread --with-readline --with-lapack --with-blas --with-proj-includes=$PREFIX/include --with-proj-data=$PREFIX/share/ --prefix=$PREFIX
 #export LD_LIBRARY_PATH=$PREFIX/lib/
 # --with-postgres=yes --with-postgres-includes=$PREFIX/include/ --with-postgres-libs=$PREFIX/lib
-./configure --with-freetype-includes=/usr/include/freetype2/ --with-geos=/opt/geos/bin/geos-config --with-netcdf=/usr/bin/nc-config --with-proj-share=/usr/share/proj --with-sqlite --with-pthread --with-readline --with-lapack=/opt/blas --with-blas=/opt/blas --prefix=/opt/grass --with-proj-includes=$PREFIX/include/ --with-proj-libs=$PREFIX/lib 
+./configure --with-freetype-includes=/usr/include/freetype2/ --with-geos=/opt/geos/bin/geos-config --with-netcdf=/usr/bin/nc-config --with-proj-share=/usr/share/proj --with-sqlite --with-pthread --with-readline=yes --with-lapack=yes --with-blas=yes --prefix=/opt/grass --with-proj-includes=$PREFIX/include/ --with-proj-libs=$PREFIX/lib 
 
 make -j $np
 make install
