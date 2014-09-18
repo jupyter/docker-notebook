2bc217180375f5d3cfac99dcd80753094fd6b704
epifanio <epiesasha@me.com>
2014-09-17 22:38:50 -0400

    Added geospatialserver container

--- geospatialserver/geonotebook.sh
+++ geospatialserver/geonotebook.sh
@@ -15,6 +15,23 @@ fi
 HASH=$(python3 -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))")
 unset PASSWORD
 
+export LD_LIBRARY_PATH=/opt/grass/grass-7.1.svn/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/opt/grass/grass-7.1.svn/etc/python:$PYTHONPATH
+export GISBASE=/opt/grass/grass-7.1.svn/
+export PATH=$PATH:$GISBASE/bin:$GISBASE/scripts
 
+export GIS_LOCK=$$
+
+mkdir -p /notebooks/grass7data
+mkdir -p /notebooks/.grass7
+
+export GISRC=/notebooks/.grass7/rc
+
+export GISDBASE=/notebooks/grass7data
+
+export GRASS_TRANSPARENT=TRUE
+export GRASS_TRUECOLOR=TRUE
+export GRASS_PNG_COMPRESSION=9
+export GRASS_PNG_AUTO_WRITE=TRUE
 
 ipython2 notebook --no-browser --port 8888 --ip=* --certfile=$PEM_FILE --NotebookApp.password="$HASH"
