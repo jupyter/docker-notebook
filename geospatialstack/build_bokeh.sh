b9e65606ddcff78c5adb4bc80dbf5dac35d47aeb
epifanio <epiesasha@me.com>
2014-10-16 19:02:32 -0400

    removed gdal build from src, using apt-get instead. added few more utilities

--- geospatialstack/build_bokeh.sh
+++ geospatialstack/build_bokeh.sh
@@ -3,7 +3,7 @@
 set -xe
 apt-get -y update
 
-ln -s /usr/bin/nodejs /usr/bin/node
+#ln -s /usr/bin/nodejs /usr/bin/node
 
 apt-get install -y node npm
 
@@ -13,6 +13,6 @@ npm install
 cd ../
 for PYTHONVER in 2 3 ; do
     PYTHON="python$PYTHONVER"
-    $PYTHON setup.py install --build_js
+    $PYTHON setup.py install #--build_js
   rm -rf build
 done
