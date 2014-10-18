b67aad7bf33df6c232060362251a4956c05aee41
epifanio <epiesasha@me.com>
2014-10-17 23:52:13 -0400

    removed gdal build from src, using apt-get instead. added few more utilities

--- geospatialstack/build_ipython_nb_extension.sh
+++ geospatialstack/build_ipython_nb_extension.sh
@@ -6,6 +6,9 @@ git clone https://github.com/ellisonbg/leafletwidget
 cd leafletwidget
 for PYTHONVER in 2 3 ; do
   PYTHON="python$PYTHONVER"
+  if PYTHONVER==2 ; then
+      pip2 install requests
+  fi
   $PYTHON setup.py install
   $PYTHON install-nbextension.py
 done
